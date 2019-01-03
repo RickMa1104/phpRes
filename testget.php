3973340292b76c2c8d9a1bba5c2a55335e8ba2ac
Rick <38975677+RickMa1104@users.noreply.github.com>
2019-01-03 22:57:21 +0800

    Add files via upload

---
+++ testget.php
@@ -0,0 +1,11 @@
+ Welcome 
+ <?php 
+	echo $_GET["name"]; 
+ ?>
+ .<br />
+ 
+You are 
+<?php 
+	echo $_GET["age"]; 
+?> 
+years old!
---
+++ testpost.php
@@ -0,0 +1,6 @@
+<?php
+header("content-type:text/html;charset=utf-8");
+echo  $_POST["name"];//可以输出form中标签的属性值   其中参数的name
+echo $_POST["age"];
+
+?>
